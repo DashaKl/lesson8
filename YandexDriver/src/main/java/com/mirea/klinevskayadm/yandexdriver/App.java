705a628e409b9864da5b07e173d8cbde45f125ee
DashaKl <darya.klin444@gmail.com>
2023-06-02 11:59:45 +0300

    first

---
+++ YandexDriver/src/main/java/com/mirea/klinevskayadm/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package com.mirea.klinevskayadm.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "286f9f00-e920-4d88-97c0-898f6c639970";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
